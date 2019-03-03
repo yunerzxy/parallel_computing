ddfede571aa16fb6a32b85de4d0c60101a73af59
yunerzxy <yunerzxy@gmail.com>
2019-03-03 13:47:23 -0800

    rank

--- hw2/mpi.cpp
+++ hw2/mpi.cpp
@@ -38,10 +38,10 @@ int main( int argc, char **argv )
     //  set up MPI
     //
     int n_proc, rank;
-    cout << "n_proc " << n_proc << " rank " << rank << endl;
     MPI_Init( &argc, &argv );
     MPI_Comm_size( MPI_COMM_WORLD, &n_proc );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    cout << "n_proc " << n_proc << " rank " << rank << endl;
     
     //
     //  allocate generic resources
