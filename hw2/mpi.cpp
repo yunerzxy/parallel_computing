c4674eb1032e9327cd521353ce8776c427125fde
yunerzxy <yunerzxy@gmail.com>
2019-03-04 11:06:08 -0800

    change

--- hw2/mpi.cpp
+++ hw2/mpi.cpp
@@ -304,12 +304,9 @@ vector<int> get_rank_neighbors(int rank) {
 //     }
 // }
 void assign_particles_to_bins(int n, double canvas_side_len, imy_particle_t *particles, vector<bin_t> &bins) {
-    //for (int i = 0; i < n; ++i) {
-    imy_particle_t &p = *particles.begins();
-    while (p != *particles.end()) {
-        int b_idx = p.particle.bin_idx = bin_of_particle(canvas_side_len, p);
-        bins[b_idx].particles.push_back(p);
-        ++p;
+    for (int i = 0; i < n; ++i) {
+        int b_idx = particles[i].particle.bin_idx = bin_of_particle(canvas_side_len, particles[i]);
+        bins[b_idx].particles.push_back(&particles[i]);
     }
 }
 
