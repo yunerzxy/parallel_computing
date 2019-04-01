be14eb0886e19f1ae84ebee24281bcf9b867df54
yunerzxy <yunerzxy@gmail.com>
2019-04-01 14:22:41 -0700

    add experiments

--- genome_assembly/experiments.sh
+++ genome_assembly/experiments.sh
@@ -1,8 +1,3 @@
-echo '------19-kmer test.txt file test result-------'
-
-dna_path=/global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/
-core=(1 2 4 8 16 32)
-
 # file=smaller/small.txt
 # for value in "${core[@]}"
 # do
@@ -31,9 +26,75 @@ core=(1 2 4 8 16 32)
 #         srun -N 1 -n $value ./kmer_hash $dna_path$file
 # done
 
-file=test.txt
+# echo '------19-kmer test.txt file test result-------'
+
+# file=test.txt
+# for value in "${core[@]}"
+# do
+# 	echo 'n = ' $value
+# 	srun -N 1 -n $value ./kmer_hash $dna_path$file
+# done
+
+dna_path=/global/project/projectdirs/mp309/cs267-spr2018/hw3-datasets/
+core=(1 2 4 8 16 32)
+node={1 2 4 8}
+
+echo '------Strong Scaling: 51-kmer large.txt file test result-------'
+
+file=large.txt
 for value in "${core[@]}"
 do
 	echo 'n = ' $value
 	srun -N 1 -n $value ./kmer_hash $dna_path$file
 done
+
+
+echo '------Strong Scaling: 51-kmer human-chr14-synthetic.txt file test result-------'
+
+file=human-chr14-synthetic.txt
+for value in "${core[@]}"
+do
+	echo 'n = ' $value
+	srun -N 1 -n $value ./kmer_hash $dna_path$file
+done
+
+
+echo '------Weak Scaling: 51-kmer large.txt file test result-------'
+
+file=large.txt
+for value in "${node[@]}"
+do
+	echo 'N = ' $value
+	srun -N $value -n 32 ./kmer_hash $dna_path$file
+done
+
+
+echo '------Weak Scaling: 51-kmer human-chr14-synthetic.txt file test result-------'
+
+file=human-chr14-synthetic.txt
+for value in "${node[@]}"
+do
+	echo 'N = ' $value
+	srun -N $value -n 32 ./kmer_hash $dna_path$file
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
